3d8985c927a0bc8840053c85fd586aca4c6d9b45
TLMatthies <timothy.l.matthies@gmail.com>
2026-05-12 21:43:14 -0700

    Added guessed_constraints.py
    Small class to store previous guesses and see if guesses are possible.
    Contains two methods: is_possible() and add_guess()
    is_possible(): takes 4 strings and returns a boolean letting you know if
    that is a possible guess, given the previous guesses and their
    correctness values.
    add_guess(): takes 4 strings and a correctness value (FullyWrong,
    OneOff, Correct) and adds constraints to the z3 solver based on the
    words and the correctness. Returns nothing.

---
+++ guessed_constraints.py
@@ -0,0 +1,103 @@
+from z3 import Solver, Sum, Int, Or, Not, And, If, sat as z3sat, ArithRef
+from enum import Enum
+
+class PrevConstraint():
+    class Correctness(Enum):
+        FullyWrong = 0
+        OneOff = 1
+        Correct = 2
+
+    def __init__(self, words: list[str]) -> None:
+        if len(words) != 16:
+            raise ValueError("Array needs a length of 16!")
+
+        self.words: dict[str, ArithRef] = {}
+        self.solver = Solver()
+
+        for word in words:
+            self.words[word] = Int(word)
+            self.solver.add(self.words[word] >= 1, self.words[word] <= 4)
+
+        self.solver.add(Sum([If(word == 1, 1, 0) for word in self.words.values()])==4)
+        self.solver.add(Sum([If(word == 2, 1, 0) for word in self.words.values()])==4)
+        self.solver.add(Sum([If(word == 3, 1, 0) for word in self.words.values()])==4)
+        self.solver.add(Sum([If(word == 4, 1, 0) for word in self.words.values()])==4)
+    
+    def is_possible(self, word1: str, word2: str, word3: str, word4: str) -> bool:
+        """
+        Check if a guess is possible, given what previous guesses we've taken and their results.
+        Example: previously guessed a, b, c, d and told completely wrong, this will return false 
+        if a, b, c, e is checked.
+        """
+        self.solver.push()
+        self.solver.add(self.words[word1] == self.words[word2],
+                        self.words[word2] == self.words[word3],
+                        self.words[word3] == self.words[word4])
+        result = (self.solver.check() == z3sat)
+        self.solver.pop()
+        return result
+
+    def add_guess(self, word1: str, word2: str, word3: str, word4: str, correctness: Correctness) -> None:
+        if not self.is_possible(word1, word2, word3, word4):
+            raise ValueError("Not a possible guess!")
+        
+        if correctness == self.Correctness.Correct:
+            self.solver.add(And(self.words[word1] == self.words[word2],
+                                self.words[word2] == self.words[word3],
+                                self.words[word3] == self.words[word4]))
+                            
+        elif correctness == self.Correctness.OneOff:
+            self.solver.add(Or(
+                And(self.words[word1] == self.words[word2], 
+                    self.words[word2] == self.words[word3],
+                    self.words[word3] != self.words[word4]),
+                And(self.words[word1] == self.words[word2],
+                    self.words[word2] == self.words[word4],
+                    self.words[word4] != self.words[word3]),
+                And(self.words[word1] == self.words[word3],
+                    self.words[word3] == self.words[word4],
+                    self.words[word4] != self.words[word2]),
+                And(self.words[word2] == self.words[word3],
+                    self.words[word3] == self.words[word4],
+                    self.words[word4] != self.words[word1])))
+        elif correctness == self.Correctness.FullyWrong:
+            self.solver.add(Not(Or(
+                And(self.words[word1] == self.words[word2], 
+                    self.words[word2] == self.words[word3],
+                    self.words[word3] != self.words[word4]),
+                And(self.words[word1] == self.words[word2],
+                    self.words[word2] == self.words[word4],
+                    self.words[word4] != self.words[word3]),
+                And(self.words[word1] == self.words[word3],
+                    self.words[word3] == self.words[word4],
+                    self.words[word4] != self.words[word2]),
+                And(self.words[word2] == self.words[word3],
+                    self.words[word3] == self.words[word4],
+                    self.words[word4] != self.words[word1]))))
+            self.solver.add(Not(And(
+                self.words[word1] == self.words[word2], 
+                self.words[word2] == self.words[word3],
+                self.words[word3] == self.words[word4]
+            )))
+            
+
+
+test1 = PrevConstraint(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'])
+test1.add_guess('a', 'b', 'c', 'd', PrevConstraint.Correctness.OneOff)
+assert test1.is_possible('a', 'b', 'c', 'd') == False
+assert test1.is_possible('a', 'b', 'c', 'e') == True
+assert test1.is_possible('a', 'b', 'e', 'f') == False
+
+test2 = PrevConstraint(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'])
+test2.add_guess('a', 'b', 'c', 'd', PrevConstraint.Correctness.FullyWrong)
+assert test2.is_possible('a', 'b', 'c', 'd') == False
+assert test2.is_possible('a', 'b', 'c', 'e') == False
+assert test2.is_possible('a', 'b', 'e', 'f') == True
+
+test3 = PrevConstraint(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'])
+test3.add_guess('a', 'b', 'c', 'd', PrevConstraint.Correctness.Correct)
+assert test3.is_possible('a', 'b', 'c', 'e') == False
+assert test3.is_possible('a', 'f', 'g', 'h') == False
+assert test3.is_possible('e', 'f', 'g', 'h') == True
+
+
