7ba4343e1658be7b8f0a73cc03859818be8b69f5
gaphilli <gaphilli@calpoly.edu>
2026-05-19 15:26:58 -0700

    Created z_weights parquet

---
+++ formZweights.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from itertools import combinations as comb
+from collections import defaultdict
+
+def load(file_name):
+    data = pd.read_parquet(file_name,
+        columns=["Game ID", "Word", "Group Name", "Group Level"])
+    data = data.dropna(subset=["Game ID", "Word", "Group Name", "Group Level"])
+    return data
+
+def save(dataset, file_name):
+    dataset.to_parquet(file_name, index=False)
+
+def main():
+    input_file = "data/Connections_Data.parquet"
+    output_file = "data/Zweights.parquet"
+
+    games = load(input_file)
+    groupings = games.groupby(["Game ID","Group Name"])
+    Z_table = defaultdict(int)
+
+    # For each unique group
+    for _, group in groupings:
+        # Get a list of the (4) words
+        words = group["Word"].tolist()
+        # Grab the 'Group Level' as a stand-in value
+        value = group["Group Level"].iloc[0]
+        # For each combination of two words
+        for word1, word2 in comb(words, 2):
+            # Force them to be alphabetical
+            a,b = sorted([word1, word2])
+            # Add the value into their stored pairing
+            Z_table[(a,b)] += 1 + value
+
+    Z_weights = pd.DataFrame([
+        (a, b, weight)
+        for (a, b), weight in Z_table.items()
+    ], columns=["Word1", "Word2", "Weight"])
+
+    save(Z_weights, output_file)
+
+
+if __name__ == "__main__":
+    main()
